30f2a3932eb445d7e449c08d661c1ee9abe66208
siddharth tamaskar <Tamaskars@siddharths-MacBook-Air.local>
2020-12-22 13:53:48 +0530

    Added a log

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log1, log2, log3, log4, log5, log6;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,6 +34,7 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(150, 200, 200, PI/2);
 
     bird = new Bird(100,100);
 
@@ -58,7 +60,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
-    bird.display();
+   bird.display();
     platform.display();
 }
